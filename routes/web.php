aff53b67f1456052a957dceffed6cc54a338bb42
NguyenHuyGiaToan <nhgtoan215@gmail.com>
2026-03-25 00:58:51 +0700

    Update: source code laravel và xóa tệp readme.md

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/vidu2','App\Http\Controllers\ViDuController@vidu2');
+Route::post('/tinhtong','App\Http\Controllers\ViDuController@tinhtong');
+
